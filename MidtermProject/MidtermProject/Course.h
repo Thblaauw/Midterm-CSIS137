7890c42086e4be8200421f0ee182351c4d13cb78
Thblaauw <thblaauw@gmail.com>
2016-10-10 21:25:48 -0700

    Course.h and Course.cpp added
    I have added the class Course and I've done the implementation on if.
    
    TODO: implementation of operator<< on Course

---
+++ MidtermProject/MidtermProject/Course.h
@@ -0,0 +1,69 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+#include "Time.h"
+#include "Date.h"
+#include <string>
+#include <iostream>
+
+//TODO: implementations of operator<<
+
+class Course {
+	
+	friend std::ostream& operator<<(std::ostream&, Course&);
+
+private:
+
+	std::string *courseNumber;
+	std::string *courseName;
+	std::string *meetingDays;
+
+	int unitCount;
+	
+	Date *startDate;
+	Date *endDate;
+
+	Time *startTime;
+	Time *endTime;
+
+public:
+	Course(std::string = "Course Number Undefined",	//courseNumber
+			std::string = "Course Name Undefined", //courseName
+			std::string = "Meeting Days Udefined", //meetingDays
+			int = 0, //unitCount
+			Date = Date(), //startDate
+			Date = Date(), //endDate
+			Time = Time(), //startTime
+			Time = Time() ); //endTIme
+
+	~Course();
+
+	double calcDailyDuration();
+
+	std::string& getCourseNumber()const;
+	Course& setCourseNumber(std::string&);
+
+	std::string& getCourseName()const;
+	Course& setCourseName(std::string&);
+
+	std::string& getMeetingDays()const;
+	Course& setMeetingDays(std::string&);
+
+	int getUnitCount()const;
+	Course& setUnitCount(int);
+
+	Date& getStartDate()const;
+	Course& setStartDate(Date&);
+
+	Date& getEndDate()const;
+	Course& setEndDate(Date&);
+
+	Time& getStartTime()const;
+	Course& setStartTime(Time&);
+
+	Time& getEndTime()const;
+	Course& setEndTime(Time&);
+
+};
+
+#endif
