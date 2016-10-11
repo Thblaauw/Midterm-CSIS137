576affb2aa27f48a3ec9a54cb5f38c2f0f7aa500
Armen Mouradyan <armenmouradyan@gmail.com>
2016-10-11 16:22:31 -0700

    Add files via upload

--- MidtermProject/MidtermProject/Date.h
+++ MidtermProject/MidtermProject/Date.h
@@ -1,8 +1,19 @@
+//Team Sky.Net
+//CSIS 137
+//Midterm Project
+
 #ifndef DATE_H
 #define DATE_H
 
+#include <iostream>
+
+using namespace std;
+
 class Date
 {
+		friend ostream& operator<<(ostream&, const Date&);
+		friend istream& operator>>(istream&, Date&);
+
         private:
                 int month; //1-12
                 int day; //1-31 depending on month
@@ -13,9 +24,21 @@ class Date
         
         public:
                Date(int = 1, int = 1, int = 1990);
+			   bool operator>(const Date&) const;
+			   bool operator<(const Date&) const;
+			   bool operator>=(const Date&) const;
+			   bool operator<=(const Date&) const;
+			   bool operator==(const Date&) const;
+			   bool operator!=(const Date&) const;
+			   int getMonth() const;
+			   int getDay() const;
+			   int getYear() const;
+			   Date& setMonth(int);
+			   Date& setDay(int);
+			   Date& setYear(int);
+			   void setDate(int, int, int);
                ~Date();
-               void setDate(int, int, int);
-               void print() const;
+               
 };
 
 #endif
